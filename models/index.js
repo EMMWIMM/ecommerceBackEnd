b30df4c79320cb61eff683d615bf497ea6b500d8
Emmalee Anderson <emilee.wimmer@gmail.com>
2022-10-16 12:42:12 -0600

    eaned up schema and started on relationships in index.js

--- models/index.js
+++ models/index.js
@@ -5,11 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+});
+
 
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'category_id' //do we need a foreign key here
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag,{
 
+})
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
